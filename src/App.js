eb68795235ce96e470733dc1b29d007e79bc1b3f
Luis Fernando <nando.lfp@hotmail.com>
2022-01-24 11:43:32 -0300

    formulario ok

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import * as yup from "yup"
 
 function App() {
 
+  const handleClickRegister = (values) => console.log(values)
+
   const handleClickLogin = (values) => console.log(values)
 
   const validationLogin = yup.object().shape({
-    email: yup.string().email("Não é um email válido").required("Este campo é obrigatório"),
-    password: yup.string().min(8, "A senha deve ter no mínimo 8 caracteres").required()
+    email: yup
+      .string()
+      .email("Não é um email válido")
+      .required("Este campo é obrigatório"),
+    password: yup
+      .string()
+      .min(8, "A senha deve ter no mínimo 8 caracteres")
+      .required("Este campo é obrigatório")
   })
+
+  const validationRegister = yup.object().shape({
+    email: yup
+      .string()
+      .email("Não é um email válido")
+      .required("Este campo é obrigatório"),
+    password: yup
+      .string()
+      .min(8, "A senha deve ter no mínimo 8 caracteres")
+      .required("Este campo é obrigatório"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "As senhas dever ser iguais")
+  })
+  
 
   return (
     <div className="container">
@@ -41,6 +64,59 @@ function App() {
             <ErrorMessage
               component="spam"
               name="password"
+              className='form-error'
+            >
+            </ErrorMessage>
+          </div>
+          <button className='button'
+          type="submit"
+          >
+            Login
+          </button>
+        </Form>
+      </Formik>
+
+      <h1>Cadastro</h1>
+      <Formik initialValues={{}}
+      onSubmit={handleClickRegister}
+      validationSchema={validationRegister}
+      >
+        <Form className="login-form">
+          <div className='login-form-group'>
+            <Field name="email"
+              className="form-field"
+              placeholder="Email"
+            >
+            </Field>
+            <ErrorMessage
+              component="spam"
+              name="email"
+              className='form-error'
+            >
+            </ErrorMessage>
+          </div>
+          <div className='login-form-group'>
+            <Field name="password"
+              className="form-field"
+              placeholder="Senha"
+            >
+            </Field>
+            <ErrorMessage
+              component="spam"
+              name="password"
+              className='form-error'
+            >
+            </ErrorMessage>
+          </div>
+          <div className='login-form-group'>
+            <Field name="confirmPassword"
+              className="form-field"
+              placeholder="Confirme sua senha"
+            >
+            </Field>
+            <ErrorMessage
+              component="spam"
+              name="confirmPassword"
               className='form-error'
             >
             </ErrorMessage>
